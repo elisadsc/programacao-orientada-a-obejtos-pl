86e931a10ebce6f155fa05c03782a44bddcbd982
Elisa Carvalho <elisa03carvalho@gmail.com>
2023-12-02 17:26:28 -0300

    Update listaClientesMaisConsumiramQuantidade.js

--- atvv-pl-typescript/src/components/listagens/listaClientesMaisConsumiramQuantidade.js
+++ atvv-pl-typescript/src/components/listagens/listaClientesMaisConsumiramQuantidade.js
@@ -14,53 +14,62 @@ export default function ListaClienteMaisConsumiramQuantidade(props) {
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>                                    Consumo 1
+                            <td>
+                                    Consumo 1
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
-                            <td>                                    Consumo 2
+                            <td>
+                                    Consumo 2
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">3</th>
-                            <td>                                    Consumo 3
+                            <td>
+                                    Consumo 3
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">4</th>
-                            <td>                                    Consumo 4
+                            <td>
+                                    Consumo 4
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">5</th>
-                            <td>                                    Consumo 5
+                            <td>
+                                    Consumo 5
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">6</th>
-                            <td>                                    Consumo 6
+                            <td>
+                                    Consumo 6
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">7</th>
-                            <td>                                    Consumo 7
+                            <td>
+                                    Consumo 7
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">8</th>
-                            <td>                                    Consumo 8
+                            <td>
+                                    Consumo 8
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">9</th>
-                            <td>                                    Consumo 9
+                            <td>
+                                    Consumo 9
                             </td>
                         </tr>
                         <tr>
                             <th scope="row">10</th>
                             <td>
-                                Consumo 10
+                                    Consumo 10
                             </td>
                         </tr>
                     </tbody>
